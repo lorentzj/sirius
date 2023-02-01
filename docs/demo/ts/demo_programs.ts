4a32226bf79277003d237acfd85d483ccfe1fe5f
Jonathan Lorentz <lorentzjonathan108@gmail.com>
2023-01-31 19:06:50 -0500

    fix demo

--- docs/demo/ts/demo_programs.ts
+++ docs/demo/ts/demo_programs.ts
@@ -1,5 +1,5 @@
 export async function load_demo_programs(): Promise<{[name: string]: string}> {
-    const pythag_source = await fetch('./demo/examples/pythag.hsp').then((response) => response.text());
+    const pythag_source = await fetch('./examples/pythag.hsp').then((response) => response.text());
     return {
         'Pythagoras': pythag_source
     }
